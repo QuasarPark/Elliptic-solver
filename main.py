ac4fc5494982ef072918a0fd374fadc7e0cb1322
QuasarPark <nslyers4018@gmail.com>
2016-05-24 20:43:44 +0900

    Directory change

--- main.py
+++
@@ -1,5 +0,0 @@
-from numpy import *
-form scipy import *
-
-
-
---
+++ src/main.py
@@ -0,0 +1,22 @@
+import scipy as np
+
+# -------------------------------------------------------------------------- #
+#
+#
+#  2D Schrodinger equation solver for elliptic boundary
+#
+#
+#
+#
+#
+#
+#
+#
+# -------------------------------------------------------------------------- #
+
+# input valuable
+
+a_major
+b_minor
+m_xx
+m_yy
